e41d469f1c1a9cd1c24a23df96dada58f8200eb3
ZGennadiy <gennadiy.zinchenko@gmail.com>
2020-09-08 08:41:50 +0300

    gh-pages

---
+++ precache-manifest.cfb38e856c9d6a64501f90b2722232c0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c784860b183707adaa4ac836f04e5eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ac20f61139bb600c86a",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "1381350688c3911cef58",
+    "url": "/static/css/main.77349307.chunk.css"
+  },
+  {
+    "revision": "1ac20f61139bb600c86a",
+    "url": "/static/js/2.a2721318.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a2721318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1381350688c3911cef58",
+    "url": "/static/js/main.6b3963b6.chunk.js"
+  },
+  {
+    "revision": "9be67e2d3b7e293ea6a8",
+    "url": "/static/js/runtime-main.c32df4f4.js"
+  },
+  {
+    "revision": "bf03f7bf43b5722fa3fdf61ec29b38c9",
+    "url": "/static/media/bg.bf03f7bf.jpg"
+  },
+  {
+    "revision": "74370baf2e60a9069de8ac970660cb27",
+    "url": "/static/media/useSearch.74370baf.svg"
+  }
+]);
--- src/components/API/api.js
+++
@@ -1,5 +0,0 @@
-export const api = {
-  key: 'a092fcaf830d4247a90130159202306',
-  http: 'http://api.weatherapi.com/v1/forecast.json?',
-  https: 'https://api.weatherapi.com/v1/forecast.json?',
-}
